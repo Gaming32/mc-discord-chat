18eb98a1aa97f966ad8c39bce080e1fd002a14db
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2022-12-04 15:37:24 -0600

    Send some things to Vanilla clients

--- src/main/java/io/github/gaming32/mcdiscordchat/DiscordChatEventListener.java
+++ src/main/java/io/github/gaming32/mcdiscordchat/DiscordChatEventListener.java
@@ -5,7 +5,9 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.network.Packet;
 import net.minecraft.network.PacketByteBuf;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import org.jetbrains.annotations.NotNull;
@@ -39,10 +41,22 @@ public class DiscordChatEventListener extends ListenerAdapter {
         }
         text.append(McDiscordChat.parseEmojis(event.getMessage().getContentRaw()));
 
+        final MutableText vanillaText = Text.empty();
+        vanillaText.append(Text.literal("[").styled(style -> style.withColor(0x2c2f33).withBold(true)));
+        vanillaText.append(Text.literal("DISCORD").styled(style -> style.withColor(0x7289da).withBold(true)));
+        vanillaText.append(Text.literal("] ").styled(style -> style.withColor(0x2c2f33).withBold(true)));
+        vanillaText.append(text);
+
         final PacketByteBuf buf = PacketByteBufs.create();
         buf.writeText(text);
-        McDiscordChat.currentServer.getPlayerManager().sendToAll(ServerPlayNetworking.createS2CPacket(
-            McDiscordChat.CHAT_DISCORD_MESSAGE, buf
-        ));
+        final Packet<?> packet = ServerPlayNetworking.createS2CPacket(McDiscordChat.CHAT_DISCORD_MESSAGE, buf);
+
+        for (final ServerPlayerEntity player : McDiscordChat.currentServer.getPlayerManager().getPlayerList()) {
+            if (ServerPlayNetworking.canSend(player, McDiscordChat.CHAT_DISCORD_MESSAGE)) {
+                player.networkHandler.sendPacket(packet);
+            } else {
+                player.sendSystemMessage(vanillaText);
+            }
+        }
     }
 }
